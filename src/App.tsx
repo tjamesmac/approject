76c9205d34c7191daa80901c3613bc84f2001847
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-24 18:43:02 +0000

    Add alternative clock function

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Clock } from './Clock'
 
+const Cloc = (props: {date: any}) => {
+  return (
+    <div>
+      <h1>This is Cloc</h1>
+      <h2>It is now {props.date.toLocaleTimeString()}</h2>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
