89a45eaf43298cd8aa9e433a84d91918c3bda2f0
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-24 19:07:19 +0000

    Remove alternative cloc

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 import { Clock } from './Clock'
 
-const Cloc = (props: {date: any}) => {
-  return (
-    <div>
-      <h1>This is Cloc</h1>
-      <h2>It is now {props.date.toLocaleTimeString()}</h2>
-    </div>
-  )
-}
-
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" id="app">
         <Clock />
       </div>
     );
