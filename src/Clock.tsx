b2682f6e4f3e2ea88d9955d4863a67e07196d412
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-24 19:07:03 +0000

    Complete stateful clock ts

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 
-export class Clock extends Component {
-  render() {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-  }
+interface IState {
+    date: Date
+}
+interface IProps {}
+export class Clock extends Component<IProps, IState> {
+    constructor(props: {timerID: IProps}) {
+        super(props);
+        this.state = {
+            date: new Date()
+        };
+    }
+    timerID: any; // This is needed for my whole op to work
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
 // export default Clock;
