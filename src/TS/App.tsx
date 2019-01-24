8c3e700abb190b932ee7c08840df76b278169409
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-24 19:11:40 +0000

    Update folder structure

--- src/TS/App.tsx
+++ src/TS/App.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../CSS/App.css';
 import { Clock } from './Clock'
 
 class App extends Component {
