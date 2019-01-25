599e0772233b15f4a016a05443d9b7d54a291e2e
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-25 18:55:35 +0000

    Add calculator test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../TS/App';
+import Calculator from '../TS/Calculator';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+it('renders without crashing calculator', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Calculator />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
