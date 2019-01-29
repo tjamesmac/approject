18e2148edadcad711b1c004fae94ab13039e3cad
Thomas McAuliffe <tjamesmac@gmail.com>
2019-01-29 07:55:08 +0000

    Add comment to pick up where I left off

--- src/TS/Workout.tsx
+++ src/TS/Workout.tsx
@@ -6,6 +6,8 @@ interface IState {
 }
 
 export const Workout = (props: {name: string, children: any})  => {
+// if the number of sets has the number of reps in each set
+// Congratulations message()
 
     return (
         <div className="workout">
